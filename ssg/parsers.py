240448e5028a8c60d7574d4cf85736a027cb22ee
Adam Burczyk <71286470+adamburczyk-dev@users.noreply.github.com>
2021-12-14 17:13:11 +0100

    ssg.py - import parsers, add parsers to config
    parsers.py - add subclass ResourceParser
    
    site.py - add run_parser and load_parser method

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,5 +1,6 @@
 from typing import List
 from pathlib import Path
+import shutil
 
 
 class Parser:
@@ -12,3 +13,21 @@ class Parser:
         raise NotImplementedError
 
     def read(self, path):
+        with open(path, "r") as file:
+            return file.read
+
+    def write(self, path, dest, content, ext=".html"):
+        full_path = self.dest / path.with_suffix(ext).name
+        with open(full_path, "r") as file:
+            file.write(content)
+
+    def copy(self, path, source, dest):
+        copy2(path, dest/path.relative_to(source))
+
+class ResourceParser extends Parser:
+    extensions = [".jpg", ".png", ".gif", ".css",".html"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
+
+    Parser.copy(path, source, dest)
