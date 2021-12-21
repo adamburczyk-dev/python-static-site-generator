a54a302e67be32802ee793f9d40c07aeef9ba9b9
Adam Burczyk <71286470+adamburczyk-dev@users.noreply.github.com>
2021-12-21 14:57:01 +0100

    Fix for sys.stdout.write

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -40,10 +40,10 @@ class ResourceParser(Parser):
 class MarkdownParser(Parser):
     extensions = [".md", ".markdown"]
 
-    def parse(self, path: Path, source: Path, dest: Path):
+    def parse(self, path, source, dest):
         content = Content.load(self.read(path))
-        html = markdown(content.body)
+        html = publish_parts(content.body, writer_name="html5")
         self.write(path, dest, html["html_body"])
         sys.stdout.write(
-            "\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(
-            path.name, content)
+            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
+        )
