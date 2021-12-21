3d25d4d37ab640b4c53a4a3c6d7543a8d514791f
Adam Burczyk <71286470+adamburczyk-dev@users.noreply.github.com>
2021-12-21 15:08:24 +0100

    w

--- ssg/site.py
+++ ssg/site.py
@@ -2,6 +2,7 @@ import sys
 
 from pathlib import Path
 
+
 class Site:
     def __init__(self, source, dest, parsers=None):
         self.source = Path(source)
@@ -11,14 +12,6 @@ class Site:
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
-
-    def build(self):
-        self.dest.mkdir(parents=True, exist_ok=True)
-        for path in self.source.rglob("*"):
-            if path.is_dir():
-                self.create_dir(path)
-            elif path.is_file():
-                self.run_parser(path)
 
     def load_parser(self, extension):
         for parser in self.parsers:
@@ -34,6 +27,13 @@ class Site:
                 "No parser for the {} extension, file skipped!".format(path.suffix)
             )
 
+    def build(self):
+        self.dest.mkdir(parents=True, exist_ok=True)
+        for path in self.source.rglob("*"):
+            if path.is_dir():
+                self.create_dir(path)
+            elif path.is_file():
+                self.run_parser(path)
 
     @staticmethod
     def error(message):
